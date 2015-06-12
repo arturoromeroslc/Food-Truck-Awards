11390c05c4f7f26e182e2b4c0d8944f88cd0db6d
Arturo <arturo.romerotcs@gmail.com>
2015-06-11 19:39:08 -0600

    vote function is WORKING!

--- layout/main/main-controller.js
+++ layout/main/main-controller.js
@@ -8,34 +8,23 @@
 		// $scope.cupbopData = cupbopData;
 		// $scope.waffluvData = waffluvData;
 		$scope.instagramUsers = MainService.getInstagramUsersArray();	
+		
 		var ref = new Firebase(fb.url);		
-		var obj = $firebaseObject(ref);
-
-		obj.$loaded()
-		  .then(function(data) {
-		  	obj.$bindTo($scope, "data");
-
-		  })
-		  .catch(function(error) {
-		    console.error("Error:", error);
-		  });
+		$scope.data = $firebaseObject(ref);
+		$scope.data.$loaded().then(function(){
+			//yay $scope.data actually equals something other than an empty object. 
+		})
 
 
 		$scope.addVotes = function(userId) {
-			var ref = new Firebase(fb.url + '/users/' + userId);		
-			var obj = $firebaseObject(ref);
-    	obj.$bindTo($scope, "data")
-    		.then(function() {
-    			console.log($scope.data.$id);
-    			if (!$scope.data.voteCount) {
-						console.log('First');
-					  $scope.data.voteCount = 1;
-					} else {
-					  	var value = parseInt($scope.data.voteCount)
-					  	var newValue = value + 1; 
-					  	return $scope.data.voteCount = newValue;	
-						}
-    		});
+			$scope.data.$loaded().then(function(){
+				$scope.data.users = $scope.data.users || {};
+				$scope.data.users[userId] = $scope.data.users[userId] || {voteCount: 0}
+				$scope.data.users[userId].voteCount = $scope.data.users[userId].voteCount + 1;
+				$scope.data.$save();
+				console.log($scope.data)
+			})
+
 		};	
 	});   
 }());    
