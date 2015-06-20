6384ba41cc806cdc2aed16366fda45fdac1cf224
Arturo <arturo.romerotcs@gmail.com>
2015-06-20 06:07:36 -0600

    carosel is two images :)

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
 			'firebase',
 			'uiGmapgoogle-maps',
 			'ui.bootstrap',
-			'ngTouch'
+			'ngTouch',
+			'ngAnimate'
 		])
 		.constant('fb', {
 			url: 'https://foodtruckawards.firebaseio.com'
@@ -39,19 +40,7 @@
     	});		
 		})
 
-		.controller('CarouselDemoCtrl', function ($scope) {
-		  $scope.myInterval = 5000;
-		  var slides = $scope.slides = [];
-		  $scope.addSlide = function() {
-		    var newWidth = 600 + slides.length + 1;
-		    slides.push({
-		      image: 'http://placekitten.com/' + newWidth + '/300',
-		      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-		        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-		    });
-		  };
-		  for (var i=0; i<4; i++) {
-		    $scope.addSlide();
-		  }
-		});
+		// .controller('CarouselDemoCtrl', function ($scope) {
+		 
+		// });
 })();
--- layout/feed/instagram-feed-ctrl.js
+++ layout/feed/instagram-feed-ctrl.js
@@ -8,8 +8,20 @@
 
 			$scope.getInstagramFeed = function(userId) {
 				MainService.getInstagramFeed(userId)
-					.then(function dataReturned(response) {
-						$scope.feedData = response;
+					.then(function dataReturned(res) {
+						var i,
+						max, 
+						a = [], 
+						b;
+						for (i = 0, max = res.length; i < max; i += 2) { 
+      				b = { image1: res[i].images.thumbnail.url };
+      				 if (res[i + 1]) {
+				       	b.image2 = res[i + 1].images.thumbnail.url;
+				       }
+				      	a.push(b);
+				      }				    
+				      $scope.groupedSlides = a;
+						console.log('array.lenght should be = 33', a)
 					})
 			}
 
@@ -18,5 +30,17 @@
 			} else if ($routeParams.instagramUser === 'waffluv') {
 				$scope.getInstagramFeed('200882079');
 				};
+
+			 $scope.myInterval = 4000;
+		   var slides = $scope.slides = [];
+		   
+		   $scope.addSlide = function() {
+		   	var newWidth = 600 + slides.length + 1;
+		  };
+		  for (var i=0; i<4; i++) {
+		    $scope.addSlide();
+		  }	
+
+
 	});   
 }()); 
