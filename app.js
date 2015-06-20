1e10be88e57826c193eeab5d365fece1918ba3f2
Arturo <arturo.romerotcs@gmail.com>
2015-06-20 04:01:49 -0600

    angular-ui

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 		.module('foodTruckApp', [
 			'ngRoute',
 			'firebase',
-			'uiGmapgoogle-maps'
+			'uiGmapgoogle-maps',
+			'ui.bootstrap',
+			'ngTouch'
 		])
 		.constant('fb', {
 			url: 'https://foodtruckawards.firebaseio.com'
@@ -36,4 +38,20 @@
         libraries: 'weather,geometry,visualization'
     	});		
 		})
+
+		.controller('CarouselDemoCtrl', function ($scope) {
+		  $scope.myInterval = 5000;
+		  var slides = $scope.slides = [];
+		  $scope.addSlide = function() {
+		    var newWidth = 600 + slides.length + 1;
+		    slides.push({
+		      image: 'http://placekitten.com/' + newWidth + '/300',
+		      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+		        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+		    });
+		  };
+		  for (var i=0; i<4; i++) {
+		    $scope.addSlide();
+		  }
+		});
 })();
